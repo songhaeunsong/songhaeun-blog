18532b573c6a5fe06b684ffa2b7c0d2f2770a0be
songhaeunsong <gkdms3469@naver.com>
2024-04-01 19:48:47 +0900

    Feat: http header 이미지 캐시 정책 정의

--- next.config.js
+++ next.config.js
@@ -2,4 +2,30 @@ const { withContentlayer } = require("next-contentlayer");
 
 module.exports = withContentlayer({
   reactStrictMode: true,
+  images: {
+    domains: ["velog.velcdn.com"],
+    formats: ["image/avif", "image/webp"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*).png",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/_next/image(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 });
