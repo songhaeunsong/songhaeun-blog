a1716e31a679b583d1ede9342ee9abab082e2caa
songhaeunsong <she3469@gmail.com>
2025-03-04 22:49:04 +0900

    chore: 주요 색상 변경

--- theme.ts
+++ theme.ts
@@ -1,6 +1,7 @@
 export const theme = {
   primaryColor: "#366d4e",
-  pointColor: "#366d4e",
+  pointColor: "#22c980",
+  deepPointColor: "#1dad6c",
   fontColor: "#474E49",
   whiteFontColor: "#e6ece8",
   transition: "background-color 0.3s ease, color 0.3s ease",
